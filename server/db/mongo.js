5e14ca07eb208171402e5c91a1243a85946d0d3e
JFrongillo <j_frongillo@salemstate.edu>
2023-10-18 00:14:38 -0400

    fixed mongodb timing out issue

--- server/db/mongo.js
+++ server/db/mongo.js
@@ -1,10 +1,11 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 const Db = process.env.DB_URL;
 
-const client = new MongoClient(Db);
-
-module.exports = () =>{
-    client.connect(process.env.DB_URL)
-    .then(() => console.log('MongoDB Connected...'))
+module.exports = () => {
+    mongoose.connect(Db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB Connected with Mongoose...'))
     .catch(err => console.log(err));
 }
